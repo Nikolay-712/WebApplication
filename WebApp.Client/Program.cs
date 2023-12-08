97af953383add23ed9caaa4f6fa7c37a8bb0ddbf
Nikolay Georgiev <nikolay--georgiev@abv.bg>
2023-12-08 12:34:02 +0200

    Add language selector

--- WebApp.Client/Program.cs
+++ WebApp.Client/Program.cs
@@ -2,6 +2,8 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
 using WebApp.Client;
 using WebApp.Client.Services;
 using WebApp.Client.Services.Implementations;
@@ -16,6 +18,16 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+IJSRuntime jsInterop = builder.Build().Services.GetRequiredService<IJSRuntime>();
+string appLanguage = await jsInterop.InvokeAsync<string>("appCulture.get");
+if (appLanguage != null)
+{
+    CultureInfo cultureInfo = new CultureInfo(appLanguage);
+    CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+    CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+}
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
